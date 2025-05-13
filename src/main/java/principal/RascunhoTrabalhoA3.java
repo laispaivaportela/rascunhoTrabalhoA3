92a2930c5cc7e7b0873bdb888fdf8fcaafee6554
Mateo-Padilla <mateo.padilla210@gmail.com>
2025-05-13 09:53:00 -0300

    Adição de classes e mudança de nome de pacote main

---
+++ src/main/java/principal/RascunhoTrabalhoA3.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+
+/**
+ *
+ * @author laispaivaportela 
+ */
+//lalalalal
+public class RascunhoTrabalhoA3 {
+
+    //bando de pobrekkk
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        // edit davi
+        //edit como que fazzzzz
+        // que infernoooooooooo
+        // VAMO TRANCAR
+        // cansei dessa vida 
+        //qualquer coisarrrrr
+        //não é possivel
+        //blablabla
+        //tomanocu essa proraaaaaa
+        // affffffffffffffffffff
+        
+        
+        //teste novamente pq ne, acontece
+        
+        
+   //clarinho nerr
+        
+        
+        //aquele teste p ver se explode ou não
+        
+        //osajkd-oddfi0rdwegagnruihngri9a0enrngé
+        // jbadvigvybdlifbcfbvfjdbvw
+        
+        
+        ///kakdsdksaksaka
+        
+        //wewwewewwweww
+        
+        //sera?
+        
+        // estoy loko ratataattatata
+        
+        
+        
+        
+        //iaidsajidsanjiasdauiushf9srs
+        
+        //uauauauauauauauua
+    }
+}
