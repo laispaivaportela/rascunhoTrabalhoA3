48de97f6700b1c94ea883f6327a0b0b3c8d3bd9f
Mateo-Padilla <mateo.padilla210@gmail.com>
2025-05-13 10:16:23 -0300

    Criação de classes com getters & setters

---
+++ src/main/java/model/Categoria.java
@@ -0,0 +1,73 @@
+package model;
+
+public class Categoria {
+
+    private String Nome;
+    private String tamP;
+    private String tamM;
+    private String tamG;
+    private String lata;
+    private String vidro;
+    private String plastico;
+
+    public Categoria() {
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getTamP() {
+        return tamP;
+    }
+
+    public void setTamP(String tamP) {
+        this.tamP = tamP;
+    }
+
+    public String getTamM() {
+        return tamM;
+    }
+
+    public void setTamM(String tamM) {
+        this.tamM = tamM;
+    }
+
+    public String getTamG() {
+        return tamG;
+    }
+
+    public void setTamG(String tamG) {
+        this.tamG = tamG;
+    }
+
+    public String getLata() {
+        return lata;
+    }
+
+    public void setLata(String lata) {
+        this.lata = lata;
+    }
+
+    public String getVidro() {
+        return vidro;
+    }
+
+    public void setVidro(String vidro) {
+        this.vidro = vidro;
+    }
+
+    public String getPlastico() {
+        return plastico;
+    }
+
+    public void setPlastico(String plastico) {
+        this.plastico = plastico;
+    }
+
+    
+}
