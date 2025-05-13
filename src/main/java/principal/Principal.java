e65aa19785e6f54ffcf8a3c041afb4303b5b8966
Mateo <158166080+Mateo-Padilla@users.noreply.github.com>
2025-05-13 10:43:35 -0300

    Update Principal.java

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package principal;
 
-/**
- *
- * @author daviw
- */
 public class Principal {
     public static void main(String[] args) {
         
