860866619caadfd5a98387e6648dca6e0b5431e4
Mateo-Padilla <mateo.padilla210@gmail.com>
2025-05-13 10:18:46 -0300

    Criação da classe produto com getters e setters

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -9,5 +9,63 @@ package model;
  * @author User
  */
 public class Produto {
+    private String Nome;
+    private double Valor;
+    private int Unidade;
+    private int QuantidadeEstoque;
+    private int QuantidadeEstoqueMinima;
+    private int QuantidadeEstoqueMaxima;
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public double getValor() {
+        return Valor;
+    }
+
+    public void setValor(double Valor) {
+        this.Valor = Valor;
+    }
+
+    public int getUnidade() {
+        return Unidade;
+    }
+
+    public void setUnidade(int Unidade) {
+        this.Unidade = Unidade;
+    }
+
+    public int getQuantidadeEstoque() {
+        return QuantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int QuantidadeEstoque) {
+        this.QuantidadeEstoque = QuantidadeEstoque;
+    }
+
+    public int getQuantidadeEstoqueMinima() {
+        return QuantidadeEstoqueMinima;
+    }
+
+    public void setQuantidadeEstoqueMinima(int QuantidadeEstoqueMinima) {
+        this.QuantidadeEstoqueMinima = QuantidadeEstoqueMinima;
+    }
+
+    public int getQuantidadeEstoqueMaxima() {
+        return QuantidadeEstoqueMaxima;
+    }
+
+    public void setQuantidadeEstoqueMaxima(int QuantidadeEstoqueMaxima) {
+        this.QuantidadeEstoqueMaxima = QuantidadeEstoqueMaxima;
+    }
+    
     
 }
